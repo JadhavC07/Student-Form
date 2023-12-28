837b3b7ca4800d45cbb008ac0e603a727ffa116c
JadhavC07 <cjaddhav@gmail.com>
2023-12-28 16:06:23 +0530

    made some changes

--- src/components/StudentRegistration.jsx
+++ src/components/StudentRegistration.jsx
@@ -68,7 +68,6 @@ const StudentRegistration = () => {
       >
         Student Registration
       </h1>
-      <div className="mb-4 text-red-500">{notification}</div>
 
       <form
         className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
@@ -239,6 +238,7 @@ const StudentRegistration = () => {
             Back
           </NavLink>
         </div>
+        <div className="mb-4 text-red-500">{notification}</div>
       </form>
     </div>
   );
