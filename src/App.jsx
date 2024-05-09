727d00e35267799e637a97580234e736a47ada3e
JadhavC07 <cjaddhav@gmai.com>
2024-05-09 17:27:58 +0530

    added hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import StudentRegistration from "./components/StudentRegistration";
+import StudentList from "./components/StudentList";
+import DashBoard from "./components/DashBoard";
 
 export default function App() {
   return (
@@ -9,6 +13,16 @@ export default function App() {
       <NavLink to="/Student-Form/registration">Regitration</NavLink> <br />
       <NavLink to="/Student-Form/all-students">Student List</NavLink> <br />
       <NavLink to="/Student-Form/dashboard">Dashboard</NavLink>
+      <Routes>
+        <Route path="/Student-Form" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/Student-Form/registration"
+          element={<StudentRegistration />}
+        />
+        <Route path="/Student-Form/all-students" element={<StudentList />} />
+        <Route path="/Student-Form/dashboard" element={<DashBoard />} />
+      </Routes>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 /* eslint-disable no-unused-vars */
 import * as React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  HashRouter,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import Login from "./components/Login";
@@ -9,31 +13,33 @@ import StudentRegistration from "./components/StudentRegistration";
 import StudentList from "./components/StudentList";
 import DashBoard from "./components/DashBoard";
 
-const router = createBrowserRouter([
-  {
-    path: "/Student-Form",
-    element: <App />,
-    children: [
-      {
-        path: "/Student-Form",
-        element: <Login />,
-      },
-      {
-        path: "/Student-Form/registration",
-        element: <StudentRegistration />,
-      },
-      {
-        path: "/Student-Form/all-students",
-        element: <StudentList />,
-      },
-      {
-        path: "/Student-Form/dashboard",
-        element: <DashBoard />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/Student-Form",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/Student-Form",
+//         element: <Login />,
+//       },
+//       {
+//         path: "/Student-Form/registration",
+//         element: <StudentRegistration />,
+//       },
+//       {
+//         path: "/Student-Form/all-students",
+//         element: <StudentList />,
+//       },
+//       {
+//         path: "/Student-Form/dashboard",
+//         element: <DashBoard />,
+//       },
+//     ],
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
